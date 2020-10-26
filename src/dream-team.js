2640f728ef239015fc827e2c3da4383fe56f5039
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-10-26 06:12:30 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -6,7 +6,7 @@ let chars = [];
 for(let i = 0; i < members.length; i++)
 {
   let word = members[i].toUpperCase();
-  if(word[0] !== '1' && word[0] !== '2' && word[0] !== '3' && word[0] !== '4' && word[0] !== '5' && word[0] !== '6' && word[0] !== '7' && word[0] !== '8' && word[0] !== '9')
+  if(word[0] !== '0' && word[0] !== '1' && word[0] !== '2' && word[0] !== '3' && word[0] !== '4' && word[0] !== '5' && word[0] !== '6' && word[0] !== '7' && word[0] !== '8' && word[0] !== '9')
   {
     chars.push(word[0]);
   }
