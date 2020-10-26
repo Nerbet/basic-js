9e4dcad65fb5de2c65feb980ac5729b840aa1020
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-10-27 02:06:05 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,14 +1,4 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  let matr = matrix.flat(Infinity);
-  let catcount = 0;
-  for(let i = 0; i < matr.length; i++)
-  {
-    if(matr[i] === "^^")
-    {
-      catcount++;
-    }
-  }
-  return catcount;
 };
